55a8e2eaec44c9f11fc478d7aa131e29421aebdc
Takahiro Kawasaki <takahiro.kawasaki0325@gmail.com>
2023-01-15 12:56:20 +0900

    feat: svgを画面に表示させる

--- lib/main_widget.dart
+++ lib/main_widget.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class MainWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(mainAxisSize: MainAxisSize.min, children: [
-      const AspectRatio(
+      AspectRatio(
         aspectRatio: 1 / 1,
-        child: Placeholder(),
+        child: SvgPicture.asset('images/sunny.svg'), // TODO: 画像の変更
       ),
       Padding(
         padding: const EdgeInsets.symmetric(vertical: 16),
